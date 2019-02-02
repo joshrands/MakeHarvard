d25f83c015a5019084b170f7534aba231b515ceb
Josh Rands <jmrands@mymail.mines.edu>
2019-02-02 12:59:20 -0500

    Single led blink test

--- ir-test.py
+++ ir-test.py
@@ -11,5 +11,22 @@ IR_LED1 = 4
 GPIO.setmode(GPIO.BCM)
 
 # setup GPIO ports
+GPIO.setup(IR_LED1, GPIO.OUT)
+
+
+try:
+	while True:
+		GPIO.output(IR_LED1, True)
+		time.sleep(1)
+
+	time.sleep(.0001)
+
+except(KeyboardInterrupt, SystemExit):
+	print("Excepted interrupt")
+	GPIO.output(IR_LED1, False)
+	exit
+finally:
+	GPIO.cleanup()
+
 
 
